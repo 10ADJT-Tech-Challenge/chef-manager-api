c0bbb35a47fea39880c839926b6978a50a56d78a
mateus barbeiro <mateusgarcia2001@gmail.com>
2025-11-26 20:27:53 -0300

    infra: inicializa projeto Spring Boot com configuração básica

---
+++ src/main/java/com/adjt/chef_manager_api/ChefManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.adjt.chef_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChefManagerApiApplication.class, args);
+	}
+
+}
