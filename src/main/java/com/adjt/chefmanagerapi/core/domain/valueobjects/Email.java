e73e7eb8dc7752b2f3d90c6f0b968bb79d7d2001
mateus barbeiro <mateusgarcia2001@gmail.com>
2026-01-03 15:34:15 -0300

    test: adiciona testes unitários para Email e melhora tratamento de exceções relacionadas
    - Implementa `EmailTest` cobrindo cenários de validação, incluindo e-mails válidos, inválidos e obrigatoriedade.
    - Refatora `EmailObrigatorioException` e `EmailInvalidoException` para centralizar mensagens de erro em constantes.
    - Atualiza regex de validação de e-mail com uso de quantificadores possessivos para maior precisão e performance.

--- src/main/java/com/adjt/chefmanagerapi/core/domain/valueobjects/Email.java
+++ src/main/java/com/adjt/chefmanagerapi/core/domain/valueobjects/Email.java
@@ -1,11 +1,18 @@
 package com.adjt.chefmanagerapi.core.domain.valueobjects;
 
+import com.adjt.chefmanagerapi.core.exceptions.EmailInvalidoException;
 import com.adjt.chefmanagerapi.core.exceptions.EmailObrigatorioException;
 
 import java.util.regex.Pattern;
 
 public record Email( String email) {
-    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+    /**
+     * Regex com Quantificadores Possessivos (++ e *+)
+     * 1. [a-zA-Z0-9_+&*-]++ : Pega a primeira parte do e-mail de forma possessiva.
+     * 2. (?:\.[a-zA-Z0-9_+&*-]++)*+ : Repetição possessiva de subpartes com ponto.
+     * 3. (?:[a-zA-Z0-9-]++\.)++ : Repetição possessiva para o domínio.
+     */
+    private static final String EMAIL_PATTERN = "^[a-zA-Z0-9_+&*-]++(?:\\.[a-zA-Z0-9_+&*-]++)*+@(?:[a-zA-Z0-9-]++\\.)++[a-zA-Z]{2,7}$";
 
     private static final Pattern PATTERN = Pattern.compile(EMAIL_PATTERN);
 
@@ -18,7 +25,7 @@ public record Email( String email) {
             throw new EmailObrigatorioException();
 
         if (!PATTERN.matcher(email).matches())
-            throw new IllegalArgumentException("Formato de email inválido. Exemplo correto: usuario@dominio.com.br");
+            throw new EmailInvalidoException();
     }
 
     @Override
